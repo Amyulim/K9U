cf220b4e76b22eb32c859ee60c67a520fc717a4c
Amyulim <apppe77@gmail.com>
2018-04-19 11:13:54 -0700

    .

--- small/pug-tutorial.js
+++ small/pug-tutorial.js
@@ -17,7 +17,8 @@ var clicked = 0;
 var foodImages = ["../SVG/leash.svg", "../SVG/comb.svg","../SVG/frisbee.svg", "../SVG/foodbag.svg", "../SVG/dogplate.svg", "../SVG/pug-completed.svg"];
 var Cprogress = 30;
 var infoG = ["../SVG/activity-mod.svg","../SVG/brush-frequency.svg","../SVG/pug-activity.svg","../SVG/empty.svg"];
-
+var myItemsArray = ["WALKING","BRUSH", "ACTIVITY","FOOD","FOOD CONSUPMSION"];
+var clickedArray = ["WALKING"];
 
 function slide() {
      if(clicked == 0){
@@ -29,7 +30,7 @@ function slide() {
         //change contents
         item1.src = foodImages[0];
         complete.innerHTML="";
-        itemName.innerHTML = "WALING";
+        itemName.innerHTML = "WALKING";
         itemDetails.innerHTML="";
         itemDetails.innerHTML =  "Use a leash when taking your dog for a walk while you are still in the training process! " ;
         infographic.src = infoG[0];
@@ -107,13 +108,25 @@ function slide() {
 
 NextArrow.addEventListener("click", function () {
     
-    document.getElementById("progress").style.width = 30+ Cprogress + "px";
-    Cprogress += 30;
+    //-1은 어레이에 그 아이템이 없음을 의미한다
+    
+    console.log(clickedArray);
+    
+    
+    
+    
     console.log(Cprogress);
     
     clicked++;   
     PrevArrow.style.display="block";
     slide();
+    
+    if(clickedArray.indexOf(itemName.innerHTML) == -1) {
+        
+        clickedArray.push(itemName.innerHTML);
+        Cprogress += 30;
+        document.getElementById("progress").style.width = Cprogress + "px";
+    }
      
 });
 
@@ -123,59 +136,84 @@ PrevArrow.addEventListener("click", function() {
 });
 
 //When top icon clicked --> change slide contens and icon
-//house.addEventListener("click", function () {
-//    
-//    clicked = 0;
-//    
-//    //hide the prev arrow
-//    PrevArrow.style.display = "none";
-//    NextArrow.style.display = "block";
-//    
-//    //change top icons
-//    house.src = "../SVG/house-colored.svg";
-//    toy.src = "../SVG/toys.svg";
-//    food.src = "../SVG/food.svg";
-//
-//    //change contents
-//    item1.src = foodImages[0];
-//    itemName.innerHTML = "LEASH";
-//    itemDetails.innerHTML =  "Use this when taking your dog for a walk while you are still in the training process! This tool helps keep your dog close beside you at all times.<br /><br />The Average walk mileage per week for Shiba Inu is <b>8 miles.</b><br />" ;
-//    
-//});
-//
-//toy.addEventListener("click", function (){
-//    
-//    clicked = 2;
-//    
-//    //change top icons
-//    house.src = "../SVG/house.svg";
-//    toy.src = "../SVG/toys-colored.svg";
-//    food.src = "../SVG/food.svg";
-//    
-//    //change contents
-//    item1.src = foodImages[2];
-//    document.getElementById("eachItemImg").style.width = "38%";
-//    itemName.innerHTML = "TOYS";
-//    itemDetails.innerHTML = "<b>40mins</b> are recommended acivities time per day.<br />Tennis Ball Rope Tug Frisbee";
-//
-//});
-//
-//food.addEventListener("click", function () {
-//    
-//    clicked = 3;
-//   
-//    //change top icons
-//    house.src = "../SVG/house.svg";
-//    toy.src = "../SVG/toys.svg";
-//    food.src = "../SVG/food-colored.svg";
-//
-//    //change contents
-//    item1.src= foodImages[3];
-//    itemName.innerHTML = "FOOD";
-//    itemDetails.innerHTML = "Like many dogs, Shiba Inus are especially attracted to meats and they love whole ingredients. Find the right dry kibble to suit this breed’s taste buds and be careful to not feed him/her too much as Shiba Inus are prone to gaining weight."
-//    
-//    
-//});
+house.addEventListener("click", function () {
+     
+    clicked = 0;
+    
+    //hide the prev arrow
+    PrevArrow.style.display = "none";
+    NextArrow.style.display = "block";
+    
+    //change top icons
+    house.src = "../SVG/house-colored.svg";
+    toy.src = "../SVG/toys.svg";
+    food.src = "../SVG/food.svg";
+
+    //change contents
+    item1.src = foodImages[0];
+    complete.innerHTML="";
+    itemName.innerHTML = "WALKING";
+    itemDetails.innerHTML="";
+    itemDetails.innerHTML =  "Use a leash when taking your dog for a walk while you are still in the training process! " ;
+    infographic.src = infoG[0]
+    
+    if(clickedArray.indexOf(itemName.innerHTML) == -1) {
+        
+        clickedArray.push(itemName.innerHTML);
+        Cprogress += 30;
+         document.getElementById("progress").style.width =  Cprogress + "px";
+    }
+});
+
+toy.addEventListener("click", function (){
+    
+    clicked = 2;
+    
+    //change top icons
+    house.src = "../SVG/house.svg";
+    toy.src = "../SVG/toys-colored.svg";
+    food.src = "../SVG/food.svg";
+
+    //change contents
+    item1.src = foodImages[2];
+    complete.innerHTML="";
+    document.getElementById("eachItemImg").style.width = "38%";
+    itemName.innerHTML = "ACTIVITY";
+    itemDetails.innerHTML = "<b>40mins</b> are recommended acivities time per day.<br />";
+    infographic.src = infoG[2];
+    
+    if(clickedArray.indexOf(itemName.innerHTML) == -1) {
+        clickedArray.push(itemName.innerHTML);
+        Cprogress += 30;
+        document.getElementById("progress").style.width =  Cprogress + "px";
+    }
+
+});
+
+food.addEventListener("click", function () {
+    
+    clicked = 3;
+   
+    //change top icons
+    house.src = "../SVG/house.svg";
+    toy.src = "../SVG/toys.svg";
+    food.src = "../SVG/food-colored.svg";
+
+    //change contents
+    item1.src= foodImages[3];
+    complete.innerHTML="";
+    itemName.innerHTML = "FOOD";
+    itemDetails.innerHTML = "Never give your pug cooked bones, as they can break easily and damage your dog's internal organs. Raw edible bones should be size appropriate for your pug and their chewing and eating habits. It;s important to teach your pug to chew bones before swallowing so you may need to hold the bone while they eat to begin with in training processes."
+    infographic.src = infoG[3];
+    
+    if(clickedArray.indexOf(itemName.innerHTML) == -1) {
+        
+        clickedArray.push(itemName.innerHTML);
+        Cprogress += 30;
+        document.getElementById("progress").style.width =  Cprogress + "px";
+    }
+    
+});
 
 document.getElementById("return").addEventListener("click",function(){
    document.getElementById("iconBoxBorder").style.left="0";
