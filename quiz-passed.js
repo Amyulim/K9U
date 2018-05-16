fd2d4b3f99b6f0acce4a4947bafb5bbdfc3ee0ba
Amyulim <apppe77@gmail.com>
2018-05-15 23:36:34 -0700

    completed
    completed version

--- quiz-passed.js
+++ quiz-passed.js
@@ -9,7 +9,7 @@ console.log(localStorage.getItem("userName"));
 //if(UserNList.length >-1){
 //    document.getElementById("passed").innerHTML =" You've passed the tutorial!";
 //} else{
-    document.getElementById("passed").innerHTML = "Great "+ UserNList + "<br/> You've passed the tutorial!";
+    document.getElementById("passed").innerHTML = "Great "+ UserNList + "<br/> You've passed the quiz!";
 //}
 
 document.getElementById("textBox").innerHTML = "<h4>Congratulations! <br>"+ UserNList+"</h4><p>You have passed the quiz and earned the title of the <b>" + DogType +" Master." + "</b> This means you know the specific requirements that are needed to take care of this breed.<br><b>Are you ready to learn about more breeds?</b></p>"
